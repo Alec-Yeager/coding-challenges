fd8713af6d6637f96013aa288381d0e01dbfea53
Alec <AYeager.Creative@protonmail.com>
2025-07-25 17:00:23 -0400

    Added cmake as the build system. Solved some more problems.

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,21 +1,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <iostream>
 #include <vector>
-template <typename T>
-void printVector(const std::vector<T> &vec)
-{
-    std::cout << "[ ";
-    for (const auto &el : vec)
-        std::cout << el << ' ';
-    std::cout << "] : len " << vec.size();
-    std::cout << std::endl;
+
+template <typename T> void printVector(const std::vector<T> &vec) {
+  std::cout << "[ ";
+  for (const auto &el : vec)
+    std::cout << el << ' ';
+  std::cout << "] : len " << vec.size();
+  std::cout << std::endl;
 }
 
-template <typename T>
-void print(T val)
-{
-    std::cout << val << std::endl;
-}
+template <typename T> void print(T val) { std::cout << val << std::endl; }
 
 #endif
